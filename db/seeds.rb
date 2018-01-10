a8a87762315db4e7887ca899d8bdef3a442c013c
Bryan Lund <bryan.helge@gmail.com>
2018-01-10 16:04:11 -0700

    added auto image to blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@ puts '1 Admin User created'
 @user = User.create(email: "regular@joe.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: "Regular Joe", roles: "user" )
 
 puts '1 User created'
+
+# 20.times do |i|
+#   Blog.
+# end
