e53f1e5359e0b19f2b087f63cbabd956a23f190c
JustimusPrime <hirejustin@yahoo.com>
2018-01-10 15:41:57 -0700

    removed type from admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-AdminUser = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: "Bob Admin", roles: "admin", type: "AdminUser")
+AdminUser = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: "Bob Admin", roles: "admin")
 
 puts '1 Admin User created'
 
