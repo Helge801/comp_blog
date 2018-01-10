246cb252958996cbefb79d5425f01f3ce66845c0
Bryan Lund <bryan.helge@gmail.com>
2018-01-10 16:17:24 -0700

    updated seeds to imclude blogs and slight adjustment to index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,41 @@
-AdminUser = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: "Bob Admin", roles: "admin")
+require 'faker'
+
+
+def get_long_content
+  Faker::HitchhikersGuideToTheGalaxy.quote +
+  Faker::HeyArnold.quote +
+  Faker::HowIMetYourMother.quote +
+  Faker::Movie.quote +
+  Faker::Seinfeld.quote +
+  Faker::Dune.quote +
+  Faker::HitchhikersGuideToTheGalaxy.quote +
+  Faker::HeyArnold.quote +
+  Faker::HowIMetYourMother.quote +
+  Faker::Movie.quote +
+  Faker::Seinfeld.quote +
+  Faker::Dune.quote +
+  Faker::HitchhikersGuideToTheGalaxy.quote +
+  Faker::HeyArnold.quote +
+  Faker::HowIMetYourMother.quote +
+  Faker::Movie.quote +
+  Faker::Seinfeld.quote +
+  Faker::Dune.quote
+end
+
+
+User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: Faker::Book.author, roles: "admin")
 
 puts '1 Admin User created'
 
-@user = User.create(email: "regular@joe.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: "Regular Joe", roles: "user" )
+User.create(email: "regular@joe.com", password: "asdfasdf", password_confirmation: "asdfasdf", name: Faker::Book.author, roles: "user" )
 
 puts '1 User created'
 
-# 20.times do |i|
-#   Blog.
-# end
+20.times do |i|
+  Blog.create!(title: Faker::Book.title,
+               body: get_long_content,
+               image: "https://picsum.photos/600/400/?image=#{[*1..800].sample}",
+               user_id: [1,2].sample
+               )
+end
+
