b95b9109fbc9a975d47e1ee5c1c42f0b970e1402
JustimusPrime <hirejustin@yahoo.com>
2018-01-10 15:28:01 -0700

    added seed data for Admin and regular User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+AdminUser = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Bob", roles: "admin", type: "AdminUser")
+
+puts '1 Admin User created'
+
+@user = User.create(email: "regular@joe.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Regular", last_name: "Joe", roles: "user" )
+
+puts '1 User created'
