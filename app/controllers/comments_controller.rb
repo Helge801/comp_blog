e93ce0afe57752bdb4532d2635a0cc24c75ee091
Bryan Lund <bryan.helge@gmail.com>
2018-01-10 15:49:15 -0700

    added comments model and built out controller (need to trim the fat)

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,61 @@
+class CommentsController < ApplicationController
+
+   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  access all: [:index, :show, :new, :edit, :create, :update, :destroy], user: :all
+
+
+  def index
+
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @comment = comment.new
+  end
+
+
+  def edit
+  end
+
+
+  def create
+    @comment = comment.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment, notice: 'comment was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'comment was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @comment.destroy
+    redirect_to comments_url, notice: 'comment was successfully destroyed.'
+  end
+
+  private
+
+    def set_comment
+      @comment = comment.find(params[:id])
+    end
+
+
+    def comment_params
+      params.require(:comment).permit(:body, :user_id, :blog_id)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110223522) do
+ActiveRecord::Schema.define(version: 20180110224252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20180110223522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +55,6 @@ ActiveRecord::Schema.define(version: 20180110223522) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
 end
